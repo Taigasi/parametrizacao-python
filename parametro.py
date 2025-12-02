b34679ae5b38ff4ebb4ada77a3bcc8fb9b2516c3
Taina Leitão <taigabriela@outlook.com>
2025-12-01 21:56:05 -0300

    primeiro commit

---
+++ parametro.py
@@ -0,0 +1,19 @@
+a = 11
+b = 15
+
+def adicao(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    return a / b
+
+print(adicao(a, b))        
+print(subtracao(a, b))     
+print(multiplicacao(a, b)) 
+print(divisao(a, b))       
---
+++ semParamentro.py
@@ -0,0 +1,24 @@
+def adicao_sem_parametros():
+    a = float(input("Digite o primeiro número: "))
+    b = float(input("Digite o segundo número: "))
+    print("Resultado da adição:", a + b)
+
+def subtracao_sem_parametros():
+    a = float(input("Digite o primeiro número: "))
+    b = float(input("Digite o segundo número: "))
+    print("Resultado da subtração:", a - b)
+
+def multiplicacao_sem_parametros():
+    a = float(input("Digite o primeiro número: "))
+    b = float(input("Digite o segundo número: "))
+    print("Resultado da multiplicação:", a * b)
+
+def divisao_sem_parametros():
+    a = float(input("Digite o primeiro número: "))
+    b = float(input("Digite o segundo número: "))
+    print("Resultado da divisão:", a / b)
+
+adicao_sem_parametros()
+subtracao_sem_parametros()
+multiplicacao_sem_parametros()
+divisao_sem_parametros()
